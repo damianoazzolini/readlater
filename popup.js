62194c68df7473d04712354f1ac7ad1f26f6d7db
Damiano Azzolini <damiano.azzolini@student.unife.it>
2019-01-30 18:35:38 +0100

    small refactor

--- popup.js
+++ popup.js
@@ -8,12 +8,6 @@ chrome.tabs.getSelected(null, function (tab) {
 });
 
 document.body.onload = function () {
-    chrome.storage.local.get("data", function (items) {
-        if (!chrome.runtime.error) {
-            console.log(items);
-            document.getElementById("data").innerText = items.data;
-        }
-    });
     var testArray = ["aaa", "bbb", "ccc","ddd"];
 
     chrome.storage.local.set({ list: testArray }, function () {
@@ -24,23 +18,22 @@ document.body.onload = function () {
     chrome.storage.local.get({ list: [] }, function (data) {
         var myJSON = JSON.stringify(data);
         var res = JSON.parse(myJSON);
-        for(var i = 0; i < res.list.length; i++) {
-            var li = document.createElement("li");
-            var but = document.createElement("button");
-            but.setAttribute('id', i);
-            but.addEventListener("click", remove);
-            // but.setAttribute('onclick', 'remove();');
-            li.setAttribute('id', i);
-            // li.appendChild(document.createTextNode(candidate.value));
-            but.appendChild(document.createTextNode('Remove'));
-            li.appendChild(document.createTextNode(res.list[i]));
-            li.appendChild(but);
-            ul.appendChild(li);
-        }
-        // var json = JSON.parse(data);
-        // document.getElementById("newdata").innerText = json.list;
-
+        generateList(res.list);
     });
+}
+
+function generateList(list) {
+    for (var i = 0; i < list.length; i++) {
+        var li = document.createElement("li");
+        var but = document.createElement("button");
+        but.setAttribute('id', i);
+        but.addEventListener("click", remove);
+        li.setAttribute('id', i);
+        but.appendChild(document.createTextNode('Remove'));
+        li.appendChild(document.createTextNode(list[i]));
+        li.appendChild(but);
+        ul.appendChild(li);
+    }
 }
 
 function remove(element) {
@@ -49,10 +42,7 @@ function remove(element) {
     chrome.storage.local.get({ list: [] }, function (data) {
         var myJSON = JSON.stringify(data);
         var res = JSON.parse(myJSON);
-        // var index = res.list.indexOf(parseInt(id,10));
-        // document.getElementById("newdata").innerText = id;
-        // document.getElementById("newdata3").innerText = index;
-        // document.getElementById("newdata4").innerText = res.list.indexOf("ccc");
+
         if(id > -1) {
             res.list.splice(id, 1);
         }
@@ -62,19 +52,7 @@ function remove(element) {
             while (ul.firstChild) {
                 ul.removeChild(ul.firstChild);
             }
-            for (var i = 0; i < res.list.length; i++) {
-                var li = document.createElement("li");
-                var but = document.createElement("button");
-                but.setAttribute('id', i);
-                but.addEventListener("click", remove);
-                // but.setAttribute('onclick', 'remove();');
-                li.setAttribute('id', i);
-                // li.appendChild(document.createTextNode(candidate.value));
-                but.appendChild(document.createTextNode('Remove'));
-                li.appendChild(document.createTextNode(res.list[i]));
-                li.appendChild(but);
-                ul.appendChild(li);
-            }
+            generateList(res.list);
             document.getElementById("newdata").innerText = res.list;
         });
     });
@@ -87,51 +65,17 @@ removeButton.onclick = function() {
 }
 */
 saveLink.onclick = function () {
-    var ul = document.getElementById("savedList");
-    var candidate = document.getElementById("candidate");
-    var li = document.createElement("li");
-    // li.setAttribute('id', candidate.value);
-    // li.appendChild(document.createTextNode(candidate.value));
-    li.appendChild(document.createTextNode(tablink));
-    ul.appendChild(li);
-    chrome.storage.local.set({ tablink: tablink }, function () {
-        //  Data's been saved boys and girls, go on home
-    });
-
-}
-
-function removeItem() {
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var item = document.getElementById(candidate.value);
-    ul.removeChild(item);
-}
-
-
-document.getElementById("set").onclick = function () {
-    var d = document.getElementById("text").value;
-
-    chrome.storage.local.set({ "data": d }, function () {
-        if (chrome.runtime.error) {
-            console.log("Runtime error.");
-        }
-    });
-
-    window.close();
-}
-
-document.getElementById("update").onclick = function () {
     chrome.storage.local.get({ list: [] }, function (data) {
-            console.log(data.list);
-            update(data.list); //storing the storage value in a variable and passing to update function
-        }
-    )
-}
-function update(array) {
-    array.push("testAdd");
-    //then call the set to update with modified value
-    chrome.storage.local.set({ list: array }, function () {
-        //console.log("added to list with new values");
-        document.getElementById("newdata").innerText = array;
+        var myJSON = JSON.stringify(data);
+        var res = JSON.parse(myJSON);
+        res.list.push(tablink);
+        chrome.storage.local.set({ list: res.list }, function () {
+            var ul = document.getElementById("savedList");
+            while (ul.firstChild) {
+                ul.removeChild(ul.firstChild);
+            }
+            generateList(res.list);
+            document.getElementById("newdata").innerText = res.list;
+        });
     });
 }

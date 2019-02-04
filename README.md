67a5a08325b15c427ee7f6375dbb0a4113d00de7
Damiano Azzolini <damiano.azzolini@student.unife.it>
2019-02-04 16:59:23 +0100

    cleaned unused stuff

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# readlater
-Chrome extension
+# Read It Later!
+This chrome extension allows you to quickly save links without using bookmarks.
+
+![Image of Yaktocat](/images/popup.png)
+
+## Two buttons
+* Save the page link
+* Remove the page link
+
+## Features
+* Add links
+* Remove links
+* Clickable links
+* That's it!
--- options.js
+++ options.js
@@ -1,15 +1,2 @@
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-        let button = document.createElement('button');
-        button.style.backgroundColor = item;
-        button.addEventListener('click', function () {
-            chrome.storage.sync.set({ color: item }, function () {
-                console.log('color is ' + item);
-            })
-        });
-        page.appendChild(button);
-    }
-}
-constructOptions(kButtonColors);
+// TODO
+// set max number of saved links
